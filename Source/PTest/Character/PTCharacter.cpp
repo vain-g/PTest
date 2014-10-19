94db311462dc8b1f4239b0867c4fa7b5c2edf65b
vain-g <2rusbekov@gmail.com>
2014-10-19 21:31:23 +0600

    19.10.2014

---
+++ Source/PTest/Character/PTCharacter.cpp
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "PTest.h"
+#include "PTCharacter.h"
+
+
+APTCharacter::APTCharacter(const class FPostConstructInitializeProperties& PCIP)
+	: Super(PCIP)
+{
+
+}
+
+
---
+++ Source/PTest/Character/PTCharacter.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Character.h"
+#include "PTCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PTEST_API APTCharacter : public ACharacter
+{
+	GENERATED_UCLASS_BODY()
+
+	
+	
+};
